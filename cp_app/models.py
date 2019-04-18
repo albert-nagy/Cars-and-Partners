ba1e5544d91877354cd5200184fe6869f6c65d6b
Albert Nagy <nagy.albert@hotmail.com>
2019-04-18 20:56:26 +0200

    Clear models

--- cp_app/models.py
+++ cp_app/models.py
@@ -1,16 +1,3 @@
 from django.db import models
-import hashlib
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    password = models.CharField(max_length=32)
-
-    def save(self, *args, **kwargs):
-        passwd_encoded = self.password.encode('utf-8')
-        m = hashlib.md5()
-        m.update(passwd_encoded)
-        self.password = m.hexdigest()
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
+# Create your models here.
