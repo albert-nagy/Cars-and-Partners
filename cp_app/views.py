eeff7e2ea749d61c704321253e2cb460a5272167
Albert Nagy <nagy.albert@hotmail.com>
2019-04-18 20:58:22 +0200

    Clear views

--- cp_app/views.py
+++ cp_app/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 
 # Create your views here.
-def login(request, name, password):
-    response = {}
-    response['name'] = name
-    response['password'] = password
-    return JsonResponse(response)
