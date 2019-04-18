c9c0d59c3e01ad6b093838118ae02128af146613
Albert Nagy <nagy.albert@hotmail.com>
2019-04-18 16:47:39 +0200

    Save password as md5

--- cp_app/models.py
+++ cp_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+import hashlib
 
 class User(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     password = models.CharField(max_length=16)
+
+    def save(self, *args, **kwargs):        
+        self.password = hashlib.md5.new(self.field).digest()
+        super().save(*args, **kwargs)
