0fc3f2c0e9f60c89b6d119bb1ea3144578acd705
Albert Nagy <nagy.albert@hotmail.com>
2019-04-18 19:44:50 +0200

    Save user with correct md5 password

--- cp_app/models.py
+++ cp_app/models.py
@@ -3,8 +3,14 @@ import hashlib
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    password = models.CharField(max_length=16)
+    password = models.CharField(max_length=32)
 
-    def save(self, *args, **kwargs):        
-        self.password = hashlib.md5.new(self.field).digest()
+    def save(self, *args, **kwargs):
+        passwd_encoded = self.password.encode('utf-8')
+        m = hashlib.md5()
+        m.update(passwd_encoded)
+        self.password = m.hexdigest()
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
