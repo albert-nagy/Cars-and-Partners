7804765188d7068f354d4a7a2cef714fdfc25f0b
Albert Nagy <nagy.albert@hotmail.com>
2019-04-20 12:58:58 +0200

    Simplify serializer

--- cp_app/serializers.py
+++ cp_app/serializers.py
@@ -4,15 +4,4 @@ from cp_app.models import Partner
 class PartnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Partner
-        fields = (
-            'id',
-            'user',
-            'name',
-            'city',
-            'address',
-            'company_name',
-            'cars',
-            'created_at',
-            'modify_at',
-            'deleted_at'
-            )
+        fields = '__all__'
