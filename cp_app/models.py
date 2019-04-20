15d4a6aa16cb234b438721f9ee81451e65b2e25c
Albert Nagy <nagy.albert@hotmail.com>
2019-04-20 11:46:48 +0200

    Add user reference to partner

--- cp_app/models.py
+++ cp_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from unixtimestampfield.fields import UnixTimeStampField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Partner(models.Model):
     id = models.IntegerField(default=0, primary_key=True)
+    user = models.ForeignKey(User, default=1)
     name = models.CharField(max_length=160)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
@@ -20,5 +22,10 @@ class Partner(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-        self.id = Partner.objects.latest('id').id +1
+        try:
+            self.id = Partner.objects.latest('id').id +1
+        except Partner.DoesNotExist:
+            self.id = 1
+        
+        
         super().save(*args, **kwargs)
