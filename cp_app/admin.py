7ae339a9637919e659f47f3bced5da6fd6009d3f
Albert Nagy <nagy.albert@hotmail.com>
2019-04-21 14:11:16 +0200

    Register Car model in admin

--- cp_app/admin.py
+++ cp_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Partner
+from .models import Partner, Car
 # Register your models here.
 admin.site.register(Partner)
+admin.site.register(Car)
