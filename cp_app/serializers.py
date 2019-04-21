e5db986ac9e2dde0a9b7a9b3cbafe2c82fafcc43
Albert Nagy <nagy.albert@hotmail.com>
2019-04-21 14:13:53 +0200

    Create CarSerializer

--- cp_app/serializers.py
+++ cp_app/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from cp_app.models import Partner
+from cp_app.models import Partner, Car
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        
+
     def create(self, validated_data):
         user = super(UserSerializer, self).create(validated_data)
         user.set_password(validated_data['password'])
@@ -16,4 +16,9 @@ class UserSerializer(serializers.ModelSerializer):
 class PartnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Partner
+        fields = '__all__'
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
         fields = '__all__'
