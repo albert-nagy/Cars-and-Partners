7259ae6ba4414f56f418fb9f93eb9f112e4715ba
Albert Nagy <nagy.albert@hotmail.com>
2019-04-23 23:32:16 +0200

    Test partner list view POST
    Try adding a new partner

--- cp_app/tests.py
+++ cp_app/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+import json
+from django.urls import reverse
+from rest_framework import status
+
+from rest_framework.test import (
+    APIClient,
+    APITestCase,
+    APIRequestFactory,
+    force_authenticate
+    )
+
+from django.contrib.auth.models import User
+from cp_app.models import Partner, Car
+from cp_app.serializers import PartnerSerializer, CarSerializer, UserSerializer
 
 # Create your tests here.
+
+class PartnerListTestCase(APITestCase):
+    
+    # response = self.client.get(url)
+
+    def setUp(self):
+        self.url = reverse("partner-list")
+        self.client = APIClient()
+
+        self.user = User.objects.create_user(
+            "someone",
+            "x@z.hu",
+            "truthisoutthere"
+            )
+        self.user.save()
+
+        self.name = "Lapos Elemér"
+        self.city = "Albertirsa"
+        self.address = "Elmebaj u. 999"
+        self.company_name = "Q Kft."
+
+        # self.Partner = Partner.objects.create(
+        #     user=self.user,
+        #     name=self.name,
+        #     city=self.city,
+        #     address=self.address,
+        #     company_name=self.company_name,
+        #     )
+
+    def test_create_partner(self):
+        """Try to create partner"""
+        data = {
+        "user": self.user.id,
+        "name": self.name,
+        "city" : self.city,
+        "address": self.address,
+        "company_name": self.company_name
+        }
+
+        # First without authentication
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Partner.objects.count(), 0)
+
+        # Then with authentication
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, data)
+
+        partner = Partner.objects.get()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Partner.objects.count(), 1)
+        self.assertEqual(partner.user_id, self.user.id)
+        self.assertEqual(partner.name, self.name)
+        self.assertEqual(partner.city, self.city)
+        self.assertEqual(partner.address, self.address)
+        self.assertEqual(partner.company_name, self.company_name)
+        self.assertGreater(partner.created_at, 0)
+        self.assertGreater(partner.modify_at, 0)
+        self.assertEqual(partner.deleted_at, 0)
+        self.assertEqual(len(partner.cars), 0)
+
+
+
+        
+
+
+
--- cp_project/settings.py
+++ cp_project/settings.py
@@ -149,4 +149,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication', 
     ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
