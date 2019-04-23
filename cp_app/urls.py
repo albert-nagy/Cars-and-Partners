3018249f2b07555a552b288b7250520585d6e9b8
Albert Nagy <nagy.albert@hotmail.com>
2019-04-23 14:32:29 +0200

    Add url names

--- cp_app/urls.py
+++ cp_app/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^add_user/$', views.UserAdd.as_view()),
+    url(r'^add_user/$', views.UserAdd.as_view(), name="add-user"),
     url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^partners/$', views.PartnerList.as_view()),
-    url(r'^partners/(\d+)/$', views.PartnerDetail.as_view()),
-    url(r'^cars/$', views.CarList.as_view()),
-    url(r'^cars/(\d+)/$', views.CarDetail.as_view()),
+    url(r'^partners/$', views.PartnerList.as_view(), name="partner-list"),
+    url(r'^partners/(\d+)/$', views.PartnerDetail.as_view(), name="partner"),
+    url(r'^cars/$', views.CarList.as_view(), name="car-list"),
+    url(r'^cars/(\d+)/$', views.CarDetail.as_view(), name="car"),
 ]
